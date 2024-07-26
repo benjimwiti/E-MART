2ebc8d8bcce5811e4557a59883793a870c11aae6
benjimwiti <167991485+benjimwiti@users.noreply.github.com>
2024-07-26 19:51:41 +0300

    disabled dev proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,9 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      "/api/": "http://localhost:8000",
-      "/uploads/profile-images/": "http://localhost:8000",
-    },
+    // proxy: {
+    //   "/api/": "http://localhost:8000",
+    //   "/uploads/profile-images/": "http://localhost:8000",
+    // },
   },
 });
