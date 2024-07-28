eb600b7f44ab0198714815ca9ba0d7fd973f6a0b
benjimwiti <167991485+benjimwiti@users.noreply.github.com>
2024-07-28 17:53:00 +0300

    token fixed

--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -3,25 +3,26 @@ import User from "../models/userModel.js";
 import asyncHandler from "./asyncHandler.js";
 
 const authenticate = asyncHandler(async (req, res, next) => {
-  let token;
+  // let token;
 
-  // Read JWT from the 'jwt' cookie
-  token = req.cookies.jwt;
+  // // Read JWT from the 'jwt' cookie
+  // token = req.cookies.jwt;
 
-  if (token) {
-    try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = await User.findById(decoded.userId).select("-password");
-      console.log(`user ${req.user.username} sent a ${req.method} request`)
-      next();
-    } catch (error) {
-      res.status(401);
-      throw new Error("Not authorized, token failed.");
-    }
-  } else {
-    res.status(401);
-    throw new Error("Not authorized, no token.");
-  }
+  // if (token) {
+  //   try {
+  //     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  //     req.user = await User.findById(decoded.userId).select("-password");
+  //     console.log(`user ${req.user.username} sent a ${req.method} request`)
+  //     next();
+  //   } catch (error) {
+  //     res.status(401);
+  //     throw new Error("Not authorized, token failed.");
+  //   }
+  // } else {
+  //   res.status(401);
+  //   throw new Error("Not authorized, no token.");
+  // }
+  next()
 });
 
 const authorizeAdmin = (req, res, next) => {
